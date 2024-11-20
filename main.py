6062abeeb313d8758f42a4c2a846c0a56fe4a11e
masha <trifooonov111606@gmail.com>
2024-11-20 10:18:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask_mysqldb import MySQL
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.secret_key = '12345abcd'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '1234'
+app.config['MYSQL_DB'] = 'kindergarten'
+mysql = MySQL(app)
+bcrypt = Bcrypt(app)
+
+@app.route('/')
+def glav_html():
+    return render_template('test.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM parents WHERE phone = %s", (username,))
+        user = cur.fetchone()
+        cur.close()
+
+        if user and bcrypt.check_password_hash(user[1], password):
+            session['username'] = username
+            flash('Вы успешно авторизовались', 'success')
+            return render_template('dashboard.html')
+        else:
+            flash('Пароль или логин неверен', 'danger')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO parents(phone, pssword) VALUES(%s, %s)", (username, hashed_password))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Вы успешно зарегестрировались', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
